4a610d532eb9119c260de9af4b68d53f4c66a5fe
GGotoEight <ggotoeight@gmail.com>
2025-01-01 19:31:59 +0300

    Add files via upload
    Создание переменных

---
+++ greatings.py
@@ -0,0 +1,15 @@
+# Creating variables
+username = "GGotoEight"
+title = "Test note"
+content = "It's a test note"
+status = "Done"
+created_date = "01-01-2025"
+issue_date = "10-01-2025"
+
+# Print variables
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания:", created_date)
+print("Дата истечения заметки (дедлайн):", issue_date, end='')
